198e3b24eb1107b9aa0f9bbf4aec8098af05ff46
Guest (AmassuOch) <chaaibi.oussama.eleve@ehtp.ac.ma>
2026-05-22 20:25:00 +0100

    Initial commit with GitHub Pages setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  // basePath is required for GitHub Pages since it will be hosted at https://wolfie4work.github.io/CM66
+  basePath: "/CM66",
+  // Optional: Change trailing slash behavior
+  trailingSlash: true,
+};
 
 export default nextConfig;
